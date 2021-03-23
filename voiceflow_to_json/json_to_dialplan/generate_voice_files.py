e4a427c7147484b47f588232c39fda491ef5c82d
Max Wilson-Hebben <zcabwil@ucl.ac.uk>
2021-03-23 08:25:16 +0000

    refactoring

--- voiceflow_to_json/json_to_dialplan/generate_voice_files.py
+++ voiceflow_to_json/json_to_dialplan/generate_voice_files.py
@@ -4,7 +4,6 @@ import librosa
 import soundfile as sf
 import sys
 
-# path = "../../asterisk_docker/conf/asterisk-build/voice"
 class GenerateVoiceFiles:
     def __init__(self, diagram_json, asterisk_sound_path):
         if getattr(sys, 'frozen', False):
@@ -51,23 +50,3 @@ class GenerateVoiceFiles:
                 file_path = self.asterisk_sound_path + "/" + node + ".wav"
                 self.create_voice_file(node_text, "en-GB", file_path)
         self.create_voice_file("Can you please repeat more clearly?", "en-GB", self.asterisk_sound_path + "/repeat.wav")
-
-
-
-# with open('/home/max/Documents/GP_IVR/voiceflow.json') as json_file:
-#     test_json = json.load(json_file)
-# test = GenerateVoiceFiles(test_json, path)
-# test.create_voice_directory()
-# test.create_voice_file("Hello World, My Name Is Max!", "en-GB", "test1.wav");
-
-
-
-# picotts = PicoTTS()
-# picotts.voice = 'en-GB'
-# wavs = picotts.synth_wav("Do you have a new persistent cough, or high temperature")
-# with open("test.wav", mode="wb") as f:
-#     f.write(wavs)
-#     f.close()
-# print(wavs)
-# wav = wave.open(io.BytesIO(wavs))
-# print (wav.getnchannels(), wav.getframerate(), wav.getnframes())
